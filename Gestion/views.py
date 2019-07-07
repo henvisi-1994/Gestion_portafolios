d0fa79d0fc89d8159f9a7e4ac0b19db81b72172f
henvisi-1994 <henvisi_1994@hotmail.com>
2019-07-07 12:55:13 -0500

    Diseño de Pagina principal
    Se realizo menu y barra lateral ademas de la pagina de inicio.

--- Gestion/views.py
+++ Gestion/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def test(request):
 	return render(request,"principal.html", {})
+def inicio(request):
+	return render(request,"inicio.html", {})
