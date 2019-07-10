eba7dc639fab200aa605b05a615487fab4c1ac80
henvisi-1994 <henvisi_1994@hotmail.com>
2019-07-10 01:00:18 -0500

    Diseño de GUI Presidente Parte 1
    Datos Informativos
    Intraclases
    Gestion de Portafolios
    Proyecto
    Distribucion de Diarios

--- Gestion/views.py
+++ Gestion/views.py
@@ -5,3 +5,13 @@ def test(request):
 	return render(request,"principal.html", {})
 def inicio(request):
 	return render(request,"inicio.html", {})
+def gestion_portafolio(request):
+	return render(request,"gestionPortafolio.html")
+def datos_informativos(request):
+	return render(request,"datosInformativos.html")
+def distribucion_diarios(request):
+	return render(request,"distribucionDiarios.html")
+def proyecto(request):
+	return render(request,"proyecto.html")
+def tareas_intraclase(request):
+	return render(request,"intraclases.html")
