4a470d31e1090e898e6e43bc06a3011107394cce
henvisi-1994 <henvisi_1994@hotmail.com>
2019-07-11 10:33:11 -0500

    Giu Matricula
    acceso al aula

--- Gestion/views.py
+++ Gestion/views.py
@@ -15,19 +15,5 @@ def proyecto(request):
 	return render(request,"proyecto.html")
 def tareas_intraclase(request):
 	return render(request,"intraclases.html")
-def tareas_extraclase(request):
-	return render(request,"extraclase.html")
-def practica_laboratorio(request):
-	return render(request,"practicaLaboratorio.html")
-def evaluaciones(request):
-	return render(request,"evaluaciones.html")
-def examenes(request):
-	return render(request,"examenes.html")
-def anexos(request):
-	return render(request,"anexos.html")
-def crear_portafolios(request):
-	return render(request,"crearPortafolio.html")
-def eliminar_portafolios(request):
-	return render(request,"eliminarPortafolio.html")
-def visualizar_portafolios(request):
-	return render(request,"visualizarPortafolio.html")
+def matricula_est(request):
+	return render(request,"matriculaEst.html")
