b85439b72482138abbf162708b170240ae875b45
henvisi-1994 <henvisi_1994@hotmail.com>
2019-07-11 21:48:11 -0500

    GUI estudiante Parte 2
    aulas virtuales
    diarios
    listaDiarios

--- Gestion/views.py
+++ Gestion/views.py
@@ -15,5 +15,27 @@ def proyecto(request):
 	return render(request,"proyecto.html")
 def tareas_intraclase(request):
 	return render(request,"intraclases.html")
+def tareas_extraclase(request):
+	return render(request,"extraclase.html")
+def practica_laboratorio(request):
+	return render(request,"practicaLaboratorio.html")
+def evaluaciones(request):
+	return render(request,"evaluaciones.html")
+def examenes(request):
+	return render(request,"examenes.html")
+def anexos(request):
+	return render(request,"anexos.html")
+def crear_portafolios(request):
+	return render(request,"crearPortafolio.html")
+def eliminar_portafolios(request):
+	return render(request,"eliminarPortafolio.html")
+def visualizar_portafolios(request):
+	return render(request,"visualizarPortafolio.html")
 def matricula_est(request):
 	return render(request,"matriculaEst.html")
+def aulas_virtuales(request):
+	return render(request,"aulasVirtuales.html")
+def diarios(request):
+	return render(request,"diarios.html")
+def lista_diarios(request):
+	return render(request,"listaDiarios.html")
